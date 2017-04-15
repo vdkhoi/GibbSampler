d74910fa91afd5ad024315b81a22292994defd6c
Khoi Vo Duy <khoi@l3s.de>
2017-04-15 22:33:20 +0200

    Fix error version

--- src/main/java/lda/model/GibbSampler.java
+++ src/main/java/lda/model/GibbSampler.java
@@ -44,8 +44,7 @@ public class GibbSampler {
 	int M; // Number of docs
 	int V; // Number of words
 	int K; // Number of topics
-	int[][] docFreqMatrix;
-
+	
 	double alpha;
 	double beta = 0.01;
 
@@ -70,7 +69,6 @@ public class GibbSampler {
 		V = docFreqMatrix.length;
 		M = docFreqMatrix[0].length;
 		K = num_topic;
-		this.docFreqMatrix = new int[M][V];
 		this.phi = new double[K][V];
 		this.theta = new double[M][K];
 		this.topicOfDoc = new int[M];
@@ -80,7 +78,6 @@ public class GibbSampler {
 		this.doc_len = new int[M];
 		this.topic_len = new int[K];
 		this.p = new double[K];
-		this.docFreqMatrix = docFreqMatrix;
 		alpha = 50.0 / K;
 		Word_Beta = V * beta;
 		Topic_Alpha = K * alpha;
@@ -101,6 +98,43 @@ public class GibbSampler {
 
 		buildMatrices();
 	}
+	
+	public void initialize_new(ArrayList<Pair>[] docFreqMatrix, int num_topic, int term_set_size) {
+		V = term_set_size;
+		M = docFreqMatrix.length;
+		K = num_topic;
+		this.phi = new double[K][V];
+		this.theta = new double[M][K];
+		this.topicOfDoc = new int[M];
+		this.topicOfWordInDoc = new ArrayList[M];
+		this.topic_word_matrix = new int[K][V];
+		this.doc_topic_matrix = new int[M][K];
+		this.doc_len = new int[M];
+		this.topic_len = new int[K];
+		this.p = new double[K];
+		alpha = 50.0 / K;
+		Word_Beta = V * beta;
+		Topic_Alpha = K * alpha;
+
+		for (int m = 0; m < M; m++) {
+			int topic = (int) Math.floor(Math.random() * K);
+			topicOfDoc[m] = topic;
+			topicOfWordInDoc[m] = new ArrayList<Pair>();
+			for (int word_in_doc = 0; word_in_doc < docFreqMatrix[m].size(); word_in_doc++) {
+				Pair word_frequency = docFreqMatrix[m].get(word_in_doc);
+				for (int count = 0; count < word_frequency.getLeft(); count++) {   //Left: frequency; Right: word
+					topic = (int) Math.floor(Math.random() * K);
+					Pair p = new Pair(topic, word_frequency.getRight()); // Left: topic. Right: word
+					topicOfWordInDoc[m].add(p);
+					topic_len[topic]++;
+					doc_len[m]++;
+				}
+			}
+		}
+
+		buildMatrices();
+	}
+	
 
 	public void buildMatrices() {
 		for (int m = 0; m < M; m++) {
@@ -126,7 +160,7 @@ public class GibbSampler {
 
 		for (int k = 0; k < K; k++) {
 			p[k] = ((topic_word_matrix[k][word] + beta) / (topic_len[topic] + Word_Beta))
-					* ((doc_topic_matrix[m][k] + alpha) / (doc_len[topic] + Topic_Alpha));
+					* ((doc_topic_matrix[m][k] + alpha) / (doc_len[m] + Topic_Alpha));
 		}
 
 		for (int k = 1; k < K; k++) {
@@ -145,7 +179,7 @@ public class GibbSampler {
 		topic_len[topic] += 1;
 		doc_len[m] += 1;
 
-		topicOfWordInDoc[m].set(n, new Pair(topic, n));
+		topicOfWordInDoc[m].set(n, new Pair(topic, word));
 
 	}
 
@@ -208,20 +242,43 @@ public class GibbSampler {
 			System.out.println("Score with d" + m + " is: " + score[m]);
 		}
 	}
+	
+	public ArrayList<Pair>[] prepare_data() {
+		ArrayList<Pair>[] doc_vector = new ArrayList[5];
+		Pair[] doc0 = {new Pair(1,0), new Pair(1,2)};  					//Left: frequency. Right: word
+		Pair[] doc1 = {new Pair(1,1), new Pair(1,2), new Pair(1,3)};  	//Left: frequency. Right: word
+		Pair[] doc2 = {new Pair(1,0), new Pair(1,3), new Pair(1,5)};                 					//Left: frequency. Right: word
+		Pair[] doc3 = {new Pair(1,4), new Pair(1,5), new Pair(1,6), new Pair(1,7)};  					//Left: frequency. Right: word
+		Pair[] doc4 = {new Pair(1,7)};                 					//Left: frequency. Right: word
+
+		doc_vector[0] = new ArrayList<Pair>(Arrays.asList(doc0));
+		doc_vector[1] = new ArrayList<Pair>(Arrays.asList(doc1));
+		doc_vector[2] = new ArrayList<Pair>(Arrays.asList(doc2));
+		doc_vector[3] = new ArrayList<Pair>(Arrays.asList(doc3));
+		doc_vector[4] = new ArrayList<Pair>(Arrays.asList(doc4));
+		
+		return doc_vector;
+	}
 
 	public static void main(String[] args) {
-		int[][] example = { { 1, 0, 1, 0, 0 }, // romeo
-				{ 1, 1, 0, 0, 0 }, // juliet
-				{ 0, 1, 0, 0, 0 }, // happy
-				{ 0, 1, 1, 0, 0 }, // dagger
-				{ 0, 0, 0, 1, 0 }, // live
-				{ 0, 0, 1, 1, 0 }, // die
-				{ 0, 0, 0, 1, 0 }, // free
-				{ 0, 0, 0, 1, 1 }, // new-hamsphire
+		int[][] example = { 
+				{ 1, 0, 1, 0, 0 }, 				// romeo
+				{ 1, 1, 0, 0, 0 }, 				// juliet
+				{ 0, 1, 0, 0, 0 }, 				// happy
+				{ 0, 1, 1, 0, 0 }, 				// dagger
+				{ 0, 0, 0, 1, 0 }, 				// live
+				{ 0, 0, 1, 1, 0 }, 				// die
+				{ 0, 0, 0, 1, 0 }, 				// free
+				{ 0, 0, 0, 1, 1 }, 				// new-hamsphire
 		};
+		
+		
 		GibbSampler model = new GibbSampler();
 
-		model.initialize(example, 2);
+		//model.initialize(example, 2);
+		
+		model.initialize_new(model.prepare_data(), 2, 8);
+		
 		model.estimate(1000);
 		model.printModel();
 		
